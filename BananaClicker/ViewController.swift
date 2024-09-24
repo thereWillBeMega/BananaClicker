66c66026cc94a162bf9be00a46ad068dcded9507
Peter Mickle <peter.mickle26@d155.org>
2024-09-24 08:06:57 -0500

    Initial Commit

---
+++ BananaClicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BananaClicker
+//
+//  Created by PETER MICKLE on 9/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BananaClickerTests/BananaClickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BananaClickerTests.swift
+//  BananaClickerTests
+//
+//  Created by PETER MICKLE on 9/24/24.
+//
+
+import Testing
+@testable import BananaClicker
+
+struct BananaClickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
