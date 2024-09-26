fc2ae7d16472af27463ad61e6e140ffbca09fcfb
Peter Mickle <peter.mickle26@d155.org>
2024-09-26 08:10:31 -0500

    shop work

--- BananaClicker/ViewController.swift
+++ BananaClicker/ViewController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+class appData{
+   static var money = 0
+   static var bananas = 10
+    static var multi = 1
+}
+
 
 class ViewController: UIViewController {
     
@@ -17,41 +23,55 @@ class ViewController: UIViewController {
     @IBOutlet weak var bananaOutlet: UILabel!
     
     var bananaState = 0
-    var money = 0
-    var bananas = 10
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        moneyOutlet.text = "money: " + String(appData.money)
+        bananaOutlet.text = "bananas: " + String(appData.bananas)
+        if appData.bananas > 0{
+            bananaState = 0
+            let curImage = UIImage(named: "banana1")
+//            sender.setImage(curImage, for: .normal)
+        }
+    }
 
 
     @IBAction func clickAction(_ sender: UIButton) {
-        if bananas > 0{
+        if appData.bananas > 0{
             if bananaState == 0{
                 bananaState = 1
                 let curImage = UIImage(named: "banana2")
-                sender.setBackgroundImage(curImage, for: .normal)
+                sender.setImage(curImage, for: .normal)
             }else if bananaState == 1{
                 bananaState = 2
                 let curImage = UIImage(named: "banana3")
-                sender.setBackgroundImage(curImage, for: .normal)
+                sender.setImage(curImage, for: .normal)
             }else if bananaState == 2{
                 bananaState = 0
                 let curImage = UIImage(named: "banana1")
-                sender.setBackgroundImage(curImage, for: .normal)
-                money += 1
-                bananas -= 1
-                moneyOutlet.text = "money: " + String(money)
-                bananaOutlet.text = "bananas: " + String(bananas)
-                if bananas == 0{
+                sender.setImage(curImage, for: .normal)
+                appData.money += 1 * appData.multi
+                appData.bananas -= 1
+                moneyOutlet.text = "money: " + String(appData.money)
+                bananaOutlet.text = "bananas: " + String(appData.bananas)
+                if appData.bananas == 0{
                     let curImage = UIImage(named: "outOfStock")
-                    sender.setBackgroundImage(curImage, for: .normal)
+                    sender.setImage(curImage, for: .normal)
                 }
             }
         }
 
         
+    }
+    
+    
+    @IBAction func shopAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "Shop", sender: self)
     }
     
     
--- BananaClicker/ViewControllerShop.swift
+++ BananaClicker/ViewControllerShop.swift
@@ -7,15 +7,50 @@
 
 import UIKit
 
-class ViewControllerShop: UIViewController {
 
+
+class ViewControllerShop: UIViewController {
+    
+    
+    @IBOutlet weak var shopMoneyOutlet: UILabel!
+    
+    @IBOutlet weak var bananaBuyOutlet: UILabel!
+    
+    @IBOutlet weak var multiBuyOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        bananaBuyOutlet.text = "10 bananas\nCost: 5"
+        
+        multiBuyOutlet.text = "Multiplier\nCost: 30"
+        
+        shopMoneyOutlet.text = "Money: " + String(appData.money)
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
+    
+    @IBAction func BananaBuyAction(_ sender: UIButton) {
+        if appData.money >= 5{
+            appData.money -= 5
+            appData.bananas += 10
+            shopMoneyOutlet.text = "Money: " + String(appData.money)
+        }
+        
+    }
+    
+    
+    @IBAction func multiBuyOutlet(_ sender: Any) {
+        if appData.money >= 30{
+            appData.money -= 30
+            appData.multi += 1
+            shopMoneyOutlet.text = "Money: " + String(appData.money)
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
